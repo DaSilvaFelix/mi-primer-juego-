89c473365c4d34ccb9b6afe5d0ac1a0e198a08a2
ElBrunoX.js <brunolok79@gmail.com>
2025-08-14 10:09:55 -0300

    fin

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import turno from "./utils/turnos.js";
 import elegirPersonajes from "./utils/elegirPersonajes.js";
 
 function iniciarCombate(jugador1, jugador2) {
-  console.log(chalk.green(`\n                                                               🔥 ¡Comienza el combate entre ${jugador1.nombre} y ${jugador2.nombre}!`));
+  console.log(chalk.green(`\n                                                         ¡Comienza el combate entre ${jugador1.nombre} y ${jugador2.nombre}!`));
 
   turno(jugador1, jugador2);
 }
