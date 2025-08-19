575d46ee8baa9ed27a17cc9334bdffe400f5a415
ElBrunoX.js <brunolok79@gmail.com>
2025-08-19 12:05:16 -0300

    corrección de emcapsulamiento

--- class/classPersonaje.js
+++ class/classPersonaje.js
@@ -1,28 +1,24 @@
 import chalk from "chalk";
 class Personaje {
   nombre;
-  vida;
-  ataque;
+  #vida;
+  #ataque;
   defensa;
   constructor(nombre, vida, ataque, defensa) {
     this.nombre = nombre;
-    this.vida = vida;
-    this.ataque = ataque;
+    this.#vida = vida;
+    this.#ataque = ataque;
     this.defensa = defensa;
   }
 
   recibirDanio(danio) {
     const danioReal = Math.max(danio - this.defensa, 0);
-    this.vida -= danioReal;
-    console.log(
-      `                                                                ${chalk.green(this.nombre)} recibe ${chalk.red(
-        danioReal
-      )} de daño. Vida restante: ${chalk.green(this.vida)}`
-    );
+    this.#vida -= danioReal;
+    console.log(`                                                                ${chalk.green(this.nombre)} recibe ${chalk.red(danioReal)} de daño. Vida restante: ${chalk.green(this.#vida)}`);
   }
 
   estaVivo() {
-    return this.vida > 0;
+    return this.#vida > 0;
   }
 }
 
